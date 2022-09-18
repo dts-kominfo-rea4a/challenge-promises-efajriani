19cf15cf847686229af118f94986b2e65d35b753
KonanKun <efajriani29@gmail.com>
2022-09-19 05:08:11 +0700

    Assignment kedua REA4A : Promise (dengan async await)

--- promise.js
+++ promise.js
@@ -1,7 +1,33 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async(emosi)=>{
+  try{
+    const TheaterIXX = await promiseTheaterIXX();
+    const TheaterVGC = await promiseTheaterVGC();
+
+    const theater = [...TheaterIXX, ...TheaterVGC];
+
+    let hitungEmosi= 0;
+
+    theater.forEach((nonton)=>{
+      if(nonton.hasil === emosi){
+        hitungEmosi++;
+      } 
+    })
+
+    return hitungEmosi;
+  }
+
+  
+
+  catch{
+    console.log("Terjadi Kesalahan ");
+  }
+  
+};
+
+
 
 module.exports = {
   promiseOutput,
